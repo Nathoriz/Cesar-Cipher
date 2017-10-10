c3aa2c05a13f33a09f91cf7c56848dadd2de3735
Nathoriz <Nathortiz_41@hotmail.com>
2017-10-09 21:08:26 -0500

    Haciendo arreglos en el codigo, html y README

--- app.js
+++ app.js
@@ -1,80 +1,112 @@
 
-var answer= prompt("Escriba una frase por favor.");
+var menu = prompt("Eliga una opcion.\n1.Encriptar mensaje.\n2.Descifrar mensaje");
 
- var array= "";
- var newArray="";
+if (menu=="1"){
+
+  var answer= prompt("Escriba una frase para convertirlo en un mensaje encriptado");
+
+ var string= "";
 
 var cipher= function(phrase) {
 
     for(i=0 ; i<phrase.length ; i++){
 
+
 if (phrase.charCodeAt(i)>=65&&phrase.charCodeAt(i)<=90){
 
-       var assci=(phrase.charCodeAt(i));
+       var assci=(((phrase.charCodeAt(i))-65+33)%26+65);
 
-       var result=((assci-65+33)%26+65);
+       var newLetter=(String.fromCharCode(assci));
 
-       var newLetter=(String.fromCharCode(result));
-
-          array+=newLetter;
+          string+=newLetter;
 
     }
-else {
-    phrase.charCodeAt(i)>=97&&phrase.charCodeAt(i)<=122;
+else if ( phrase.charCodeAt(i)>=97&&phrase.charCodeAt(i)<=122) {
 
-     var assci2 = (phrase.charCodeAt(i));
+       var assci2=(((phrase.charCodeAt(i))-97+33)%26+97);
 
-       var result2=((assci2-97+33)%26+97);
+       var newLetter2=(String.fromCharCode(assci2));
 
-       var newLetter2=(String.fromCharCode(result2));
+          string+=newLetter2;
 
-          array+=newLetter2;
+}else {
+
+   if(phrase.charCodeAt(i)==32){
+       alert("No deje espacios en la frase la proxima vez. =) ");
+
+
+     }
+
+     else if(phrase.charCodeAt(i)>=48&&phrase.charCodeAt(i)<=57){
+       alert("No escriba numeros en la frase la proxima vez, por que no podra encriptarlo. :) ");
+
+
+     }
 
 }
+
     }
 
-return array;
+alert(string);
     }
 
 
 cipher(answer);
 
-console.log(array);
+}
+else if (menu=="2") {
+
+  var answer2= prompt("Introdusca el mensaje encriptado para convertilo en una frase.");
+var string2="";
+
+var decipher= function(phrase2) {
+
+   for(i=0 ; i<phrase2.length ; i++){
+
+if (phrase2.charCodeAt(i)>=65&&phrase2.charCodeAt(i)<=90){
 
 
-var decipher= function(array) {
+       var assci=(((phrase2.charCodeAt(i))+65-33)%26+65);
 
-   for(i=0 ; i<array.length ; i++){
+       var newLetter=(String.fromCharCode(assci));
 
-if (array.charCodeAt(i)>=65&&array.charCodeAt(i)<=90){
-
-       var assci=(array.charCodeAt(i));
-
-       var result=((assci-33)%26);
-
-       var newLetter=(String.fromCharCode(result));
-
-          newArray+=newLetter;
+          string2+=newLetter;
 
     }
-else {
-    array.charCodeAt(i)>=97&&array.charCodeAt(i)<=122;
+ else if (phrase2.charCodeAt(i)>=97&&phrase2.charCodeAt(i)<=122) {
 
-    var assci2= (array.charCodeAt(i));
+       var assci2=((phrase2.charCodeAt(i))%26+97);
 
-       var result2=((assci2-33)%26);
+       var newLetter2=(String.fromCharCode(assci2));
 
-       var newLetter2=(String.fromCharCode(result2));
+          string2+=newLetter2;
 
-          newArray+=newLetter2;
+}else{
+
+ if(phrase2.charCodeAt(i)==32){
+       alert("No deje espacios en la frase en la proxima vez. =) ");
+
+     }
+
+     else if(phrase2.charCodeAt(i)>=48&&phrase2.charCodeAt(i)<=57){
+       alert("No escriba numeros en la frase porque no podra descifrarlo. :) ");
+
+     }
+}
+
+
+    }
+
+alert (string2);
+    }
+
+
+decipher(answer2);
+
+
+} else {
+  menu!=="1"||menu!=="2";
+
+  alert("Por favor eliga el numero de las opciones existentes. =( ");
 
 }
-    }
-
-return newArray;
-    }
-
-console.log(newArray);
-
-
-decipher(array);
